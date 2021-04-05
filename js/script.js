abb29aa623db7e50f7870fbfbba93126d5b5c0a5
rovozita <rovozita@gmail.com>
2021-04-05 16:10:15 +0200

    Made my rocket!

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// this is a comment.
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById('cool').className;
+
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+
+}
+
 var sayMyName = function (name) {
 	alert ('My name is: '+name);
 }
